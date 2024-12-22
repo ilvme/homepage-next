215146f42259c85da743525589bb963e918600ed
ilvme <ikangjia.cn@outlook.com>
2024-12-22 15:50:00 +0800

    feat: 友情链接支持

---
+++ .vitepress/components/Friends.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { friends } from '../../scripts/friends.js'
+const to = (url) => {
+  window.open(url)
+}
+</script>
+
+<template>
+  <div v-if="friends.length === 0">空空如也～</div>
+  <div class="friends" v-else>
+    <div @click="to(item.link)" class="friends-item" v-for="item in friends" :key="item.link">
+      <div style="display: flex; justify-content: center; align-items: center; gap: 10px">
+        <img :src="item.icon" :alt="item.title" class="avatar" />
+        <div class="title">{{ item.title }}</div>
+      </div>
+      <div class="desc">{{ item.desc }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.friends {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  margin-top: 16px;
+}
+.friends-item {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  width: 300px;
+  height: 120px;
+  padding: 10px 16px;
+  border: 1px solid var(--vp-code-copy-code-hover-border-color);
+  cursor: pointer;
+  border-radius: 8px;
+}
+.friends-item:hover {
+  background: var(--vp-c-bg-alt);
+}
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+.title {
+  margin-top: 8px;
+  color: var(--vp-c-text-1);
+  text-align: center;
+  font-size: 1.15rem;
+  font-weight: bold;
+}
+.desc {
+  margin-top: 8px;
+  color: #666;
+  font-size: 0.75rem;
+  line-height: 1.5;
+}
+</style>
--- docs/friends.md
+++ docs/friends.md
@@ -1,11 +1,39 @@
 ---
-
+title: 友情链接
+outline: false
+aside: false
+sidebar: false
+footer: false
 ---
-<script setup>
 
-const partners = [
-    {}
-];
+<script setup>
+import Friends from '../.vitepress/components/Friends.vue';
 </script>
 
 # 友情链接
+
+
+::: details 申请友链
+
+**友链要求:**
+
+- 网站应保持清洁，避免过多广告内容
+- 网站需要有良好的稳定性和可靠性
+
+**申请方式：**
+1. 直接在本站下方留言 <badge type="warning" text="评论功能开发中..."/>
+2. 给我发 [Email](mailto:ikangjia.cn@outlook.com)
+
+**格式参考及本站信息如下：**
+
+```json
+{
+    "title": "林深时觉寒",
+    "desc": "越来越明白，长大后的生活，没有喜出望外的惊喜，也没有自然而然的稳定前进，有的只不过是需要带上披荆斩棘般的勇气去开拓！",
+    "link": "https://github.com/jaywcjlove/front-end-learning",
+    "icon": "https://ikangjia.cn/images/avatar1.jpeg"
+}
+```
+:::
+
+<Friends/>
