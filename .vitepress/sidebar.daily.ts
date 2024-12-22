9a8891ab72463baaa5a84f6f69ea1ac1d4224646
ilvme <ikangjia.cn@outlook.com>
2024-12-22 10:16:02 +0800

    docs: 集成部分旧博客...

--- .vitepress/sidebar.daily.ts
+++ .vitepress/sidebar.daily.ts
@@ -3,10 +3,7 @@ const DAILY = {
     {
       text: '2024 年',
       collapsed: false,
-      items: [
-        { text: '躺平与内卷之杂谈（味儿冲&慎读）', link: '/daily/躺平与内卷之杂谈.md' },
-        { text: '梦偶天书', link: '/daily/mess/梦偶天书.md' },
-      ],
+      items: [{ text: '梦偶天书', link: '/daily/mess/梦偶天书.md' }],
     },
     {
       text: '2023 年',
@@ -17,22 +14,72 @@ const DAILY = {
         { text: '猫油诗', link: '/daily/poetry/猫油诗.md' },
       ],
     },
+
+    {
+      text: '2022 年',
+      collapsed: false,
+      items: [
+        {
+          text: '关于写年终总结这件小事',
+          link: '/daily/mess/关于写年终总结这件小事.md',
+        },
+        { text: '怪象论（未成理论）', link: '/daily/thoughts/怪象论（未成理论）.md' },
+        {
+          text: '为人不识鹿鼎公，再称风流也枉然',
+          link: '/daily/essay/为人不识鹿鼎公，再称风流也枉然.md',
+        },
+        { text: '买书是一件很矫情的事情', link: '/daily/essay/买书是一件很矫情的事情.md' },
+        { text: 'Flomo 替代计划', link: '/daily/mess/Flomo 替代计划.md' },
+        {
+          text: '自我管理（三）：探索更加贴合实际的二代模型',
+          link: '/daily/thoughts/自我管理（三）：探索更加贴合实际的二代模型.md',
+        },
+        {
+          text: '自我管理（二）：初代理论模型 PMT-7 的探索之路',
+          link: '/daily/thoughts/自我管理（二）：初代理论模型 PMT-7 的探索之路.md',
+        },
+        {
+          text: '自我管理（一）：意识的启蒙',
+          link: '/daily/thoughts/自我管理（一）：意识的启蒙.md',
+        },
+      ],
+    },
+
     {
       text: '2021 年',
       collapsed: false,
-      items: [{ text: '2021 年小结（完整版）', link: '/daily-notes/2021/2021 年小结（完整版）' }],
+      items: [
+        { text: '2021 年小结（完整版）', link: '/daily-notes/2021/2021 年小结（完整版）' },
+        { text: '躺平与内卷之杂谈（味儿冲&慎读）', link: '/daily/thoughts/躺平与内卷之杂谈.md' },
+      ],
+    },
+
+    {
+      text: '2019 年',
+      collapsed: false,
+      items: [
+        { text: '用了近十年的 QQ', link: '/daily/essay/用了近十年的 QQ.md' },
+        { text: '雨中', link: '/daily/poetry/雨中.md' },
+        { text: '月圆', link: '/daily/poetry/月圆.md' },
+      ],
     },
 
     {
       text: '2018 年',
       collapsed: false,
-      items: [{ text: '北京路上', link: '/daily/essay/北京路上.md' }],
+      items: [
+        { text: '北京路上', link: '/daily/essay/北京路上.md' },
+        { text: '不成熟的梦想', link: '/daily/essay/不成熟的梦想.md' },
+      ],
     },
 
     {
       text: '2016 年',
       collapsed: false,
-      items: [{ text: '大学思修论文一篇', link: '/daily/essay/大学思修论文一篇.md' }],
+      items: [
+        { text: '窗', link: '/daily/poetry/窗.md' },
+        { text: '大学思修论文一篇', link: '/daily/essay/大学思修论文一篇.md' },
+      ],
     },
 
     {
